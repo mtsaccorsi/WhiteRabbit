51abac54f994578c53012f6931a8829b0a784fe0
mtsaccorsi <mts.accorsi@gmail.com>
2023-01-03 21:58:48 -0300

    Initial Commit

---
+++ WhiteRabbit/WhiteRabbitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhiteRabbitApp.swift
+//  WhiteRabbit
+//
+//  Created by Matheus Accorsi on 03/01/23.
+//
+
+import SwiftUI
+
+@main
+struct WhiteRabbitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
